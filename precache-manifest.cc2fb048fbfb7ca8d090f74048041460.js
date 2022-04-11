6631cf53948dade678a410b14c2fb5744d3f0188
flaviofsb <flavio@afixo.com.br>
2022-04-11 11:38:38 -0300

    Deploy netlfy

---
+++ precache-manifest.cc2fb048fbfb7ca8d090f74048041460.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19f0e5900ec04d80151",
+    "url": "/css/chunk-193e5834.e0b315e9.css"
+  },
+  {
+    "revision": "19e3a5f4aaed19637d52",
+    "url": "/css/chunk-1fe98eac.75232888.css"
+  },
+  {
+    "revision": "a5b1b2a5e38de531aaea",
+    "url": "/css/chunk-31c2af2e.f0222215.css"
+  },
+  {
+    "revision": "50eaa93ad22de42209cd",
+    "url": "/css/chunk-672a0add.447bbd03.css"
+  },
+  {
+    "revision": "1344bfd8efa6086edeb1",
+    "url": "/css/chunk-affd6f2a.8fd1e4b7.css"
+  },
+  {
+    "revision": "954a817f82306a059b13",
+    "url": "/css/chunk-vendors.c439af37.css"
+  },
+  {
+    "revision": "abb9675266500c452386065ff2e4a005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "487f0e110c0f8495142d",
+    "url": "/js/app.808fadfd.js"
+  },
+  {
+    "revision": "c19f0e5900ec04d80151",
+    "url": "/js/chunk-193e5834.abc73bd1.js"
+  },
+  {
+    "revision": "19e3a5f4aaed19637d52",
+    "url": "/js/chunk-1fe98eac.36dbc2de.js"
+  },
+  {
+    "revision": "a5b1b2a5e38de531aaea",
+    "url": "/js/chunk-31c2af2e.b13cf974.js"
+  },
+  {
+    "revision": "50eaa93ad22de42209cd",
+    "url": "/js/chunk-672a0add.be6099fc.js"
+  },
+  {
+    "revision": "1344bfd8efa6086edeb1",
+    "url": "/js/chunk-affd6f2a.1730c699.js"
+  },
+  {
+    "revision": "954a817f82306a059b13",
+    "url": "/js/chunk-vendors.6355972e.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
